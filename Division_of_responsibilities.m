533f97022758601f0d536ea482e13ad9520df165
jeguzmanmd <70674157+jeguzmanmd@users.noreply.github.com>
2020-10-07 16:57:32 -0400

    Update with variable names

--- Division_of_responsibilities.m
+++ Division_of_responsibilities.m
@@ -3,3 +3,14 @@
 % Rebecca: initialize the simulation (box, particles)
 % Ezra wuz here
 % Jorge Got it
+%   Variable names
+%               r = distance between particles
+%               rred = reduced distance
+%               sigma = diameter of the particle
+%               epsilon = well depth = Energy
+%               U = potential energy
+%               Ured = reduced energy
+%               F = Force
+%               Fred = reduced force
+%               RCoff = cut off radius
+%               npart = number of particles

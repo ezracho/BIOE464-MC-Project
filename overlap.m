cdccc84cb01e12d3d91598cb7f6d862585c4e133
ezracho <70588799+ezracho@users.noreply.github.com>
2020-10-08 10:24:01 -0400

    First draft of overlap function
    Likely use within function to generate points

---
+++ overlap.m
@@ -0,0 +1,14 @@
+function [repeat] = overlap(gen, R)
+% Returns boolean on whether to repeat or not; uses row vectors
+% gen = generated array, R = radius of particles; testing uses R = 0.0001                                 
+k = size(gen, 1);                                     % Finds number of points; determines iterations
+if k > 1 
+    for g = 1:k - 1
+        [x, y, z] = gen(g, :) - gen(k, :);          % assuming generated as column vectors; assuming final added point is at the end of matrix
+        dist = sqrt(x^2 + y^2 + z^2); 
+        if dist < 2*R;                              % Overlap occurs if two are closer than adjacent (r/2 + r/2)
+            repeat = true;                          % Repeat if overlap occurs 
+            break
+        end
+    end
+end

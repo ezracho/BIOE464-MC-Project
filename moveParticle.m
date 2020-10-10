fa91371f7e03771eef634a9a0b94e6645a596950
ezracho <ezracho2000@gmail.com>
2020-10-10 14:51:07 -0400

    Updated Overlap Changed some syntax for placing_particles; also added plot for visualization

--- moveParticle.m
+++ moveParticle.m
@@ -1,3 +1,10 @@
-function [true] = moveParticle(inputs)
-% Function to move particles if conditions are met
+function [fPos] = moveParticle(oPos, nPos, )
+% Function to move particles if conditions are met.  Creates a "final"
+% position matrix (doing this wholesale, because why not; may change to
+% doing individual points
+
+
+
+
+
 
--- overlap.m
+++ overlap.m
@@ -5,8 +5,8 @@ k = size(gen, 1);                                     % Finds number of points; 
 repeat = false;
 if k > 1 
     for g = 1:k - 1
-        [x, y, z] = gen(g, :) - gen(k, :);          % assuming generated as column vectors; assuming final added point is at the end of matrix
-        dist = sqrt(x^2 + y^2 + z^2); 
+        pos = gen(g, :) - gen(k, :);          % assuming generated as column vectors; assuming final added point is at the end of matrix
+        dist = sqrt(pos(1)^2 + pos(2)^2 + pos(3)^2); 
         if dist < 2*R;                              % Overlap occurs if two are closer than adjacent (r/2 + r/2)
             repeat = true;                          % Repeat if overlap occurs 
             break

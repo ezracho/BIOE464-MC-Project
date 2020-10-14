2a3a0069fc94b6e9e137fe14c4553a71f60f41d5
rebeccavaudreuil <70704923+rebeccavaudreuil@users.noreply.github.com>
2020-10-14 17:49:28 -0400

    changed dist to reflect periodic boundary conditions

--- Dist.m
+++ Dist.m
@@ -1,10 +1,16 @@
+%to implement periodic boundary conditions
 function[r]=Dist(pos1,pos2,L)
-r=zeros(length(pos1),1);
-for i =1:length(pos1);
-    r(i) = sqrt(sum((pos1(i)-pos2).^2));
-    if abs(r(i))>(L/2)
-        r(i)=L-r(i);
-    else
-        r(i)=r(i);
+%%set quantity to shift by
+rcut = L/2; %max distance before particle is further than an image
+
+%%for loop to shift to nearest image
+r_vector = [] %initialize vector form of distance
+for j = 1:3 %for each dimension
+    if abs(pos1(j)-pos2(j)) > rcut %if particles are further than rcut away in this dimension
+        pos2(j) = pos2(j) - rcut; %then shift by rcut
+    %else keep position the same
+    end
+    r_vector = [r_vector, abs(pos1(j)-pos2(j))]; %add this dimension to distance vector
 end
-end
+
+r = sqrt(sum(r_vector.^2)); %scalar value of radius

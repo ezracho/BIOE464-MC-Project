ac07007e92d77ea619a0847f65f7ec0dd173d825
ezracho <ezracho2000@gmail.com>
2020-10-18 16:54:14 -0400

    Messed around with variables; both have debugging code

--- MC_Project_Pipeline.m
+++ MC_Project_Pipeline.m
@@ -10,10 +10,10 @@
     % While considering periodic boundary conditions (Rebecca)
 
 % Start by declaring space.  Change values as needed
-N = 500; R = 0.0001; L = 1; T = 100;        % Declare number of particles, radius of particles, and space length (L x L x L)
-% Setting T = 100K for now
-sigma = 1; epsilon  = 1;                    % Cutoff distance, well depth
-iterations = 50;                            % Setting iterations as 10 times for now    
+N = 500; R = 0.001; L = 1; T = 2.0;        % Declare number of particles, radius of particles, and space length (L x L x L)
+% Setting T = 2.0 K for now
+sigma = 1.5; epsilon  = 0.001;                    % Cutoff distance, well depth
+iterations = 1000;                            % Setting iterations as 10 times for now    
 
 %% Create old particles
 old_positions = placing_particles(N, R, L);                         % Creates old positions
@@ -32,17 +32,17 @@ for i = 1:iterations
         old_positions = new_positions;
         U_old = U_new;
     end
-    alt_pos_hist = [alt_pos_hist; old_positions(1, :)]              % Allows to check if it changes
+    alt_pos_hist = [alt_pos_hist; old_positions(1, :)];              % Allows to check if it changes
     pos_hist = [pos_hist; old_positions];                           % History by appending positions
         % But like it's 100 positions each time, so...
     U_hist = [U_hist; U_old];
     %%% < Insert line to append to another file >
     i = i + 1;
 end
-pos_hist
-U_hist
-plot(U_hist)
-
+% subplot(1, 2, 1);
+plot(U_hist);
+maxU = max(U_hist); ylim([-0.2*maxU, maxU]); 
+% subplot(1, 2, 2); plot(alt_pos_hist(:, 1));
 
 
 

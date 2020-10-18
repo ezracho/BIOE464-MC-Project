fac00108839859651fbbd2fa469a4e4d3c7bfe63
ezracho <ezracho2000@gmail.com>
2020-10-18 15:52:52 -0400

    Removed loop in LJP

--- LJP.m
+++ LJP.m
@@ -1,7 +1,2 @@
 function [U2] = LJP(epsilon,sigma,r)
-
-for i=1:length(r)
- 
-    U2= 4.*epsilon.*((sigma./r).^12 - (sigma./r).^6);
-
-end
+U2= 4.*epsilon.*((sigma./r).^12 - (sigma./r).^6);
--- MC_Project_Pipeline.m
+++ MC_Project_Pipeline.m
@@ -13,30 +13,35 @@
 N = 500; R = 0.0001; L = 1; T = 100;        % Declare number of particles, radius of particles, and space length (L x L x L)
 % Setting T = 100K for now
 sigma = 1; epsilon  = 1;                    % Cutoff distance, well depth
-iterations = 10;                            % Setting iterations as 10 times for now    
+iterations = 50;                            % Setting iterations as 10 times for now    
 
 %% Create old particles
 old_positions = placing_particles(N, R, L);                         % Creates old positions
-U_old = testw_alt(N, L, T, old_positions, sigma, epsilon);          % Insert old energy calculation
+[U_old, vir_old] = testw_alt(N, L, T, old_positions, sigma, epsilon);          % Insert old energy calculation
 pos_hist = old_positions;                                         % History of positions
 U_hist = U_old;                                                   % History of energy
+% Do something about "vir" (old and new)
+    alt_pos_hist = [];                                              % For debugging
                     %%% < Insert line to append to another file > %%%
 %% Create a loop for new positions
 for i = 1:iterations
     new_positions = placing_particles(N, R, L); 
-    U_new = testw_alt(N, L, T, new_positions, sigma, epsilon); % insert new energy calculation
+    [U_new, vir_new] = testw_alt(N, L, T, new_positions, sigma, epsilon); % insert new energy calculation
     update = updateCheck(U_old, U_new, T); 
     if update == true
         old_positions = new_positions;
         U_old = U_new;
     end
+    alt_pos_hist = [alt_pos_hist; old_positions(1, :)]              % Allows to check if it changes
     pos_hist = [pos_hist; old_positions];                           % History by appending positions
+        % But like it's 100 positions each time, so...
     U_hist = [U_hist; U_old];
     %%% < Insert line to append to another file >
     i = i + 1;
 end
-
-
+pos_hist
+U_hist
+plot(U_hist)
 
 
 

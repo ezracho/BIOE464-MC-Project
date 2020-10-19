e45c2acbce676395e339359c2b64ec6dbfadd376
ezracho <ezracho2000@gmail.com>
2020-10-19 17:38:55 -0400

    Question 1 stuff

--- MC_Project_Pipeline.m
+++ MC_Project_Pipeline.m
@@ -11,10 +11,10 @@ function [pos_hist, P_hist, vir_hist, U_hist] = MC_Project_Pipeline(N, R, L, T, 
     % While considering periodic boundary conditions (Rebecca)
 % σ = 3.4 × 10-10 m and ε = 1.65 × 10-21
 % Start by declaring space.  Change values as needed
-N = 500; R = 0.001; L = 2; T = 300;        % Declare number of particles, radius of particles, and space length (L x L x L)
+%%%N = 500; R = 0.001; L = 2; T = 300;        % Declare number of particles, radius of particles, and space length (L x L x L)
 % Setting T = 2.0 K for now
-sigma = 0.01; epsilon  = 0.1;                    % Cutoff distance, well depth
-iterations = 1000;                            % Setting iterations as 10 times for now    
+%%% sigma = 0.01; epsilon  = 0.1;                    % Cutoff distance, well depth
+%%% iterations = 1000;                            % Setting iterations as 10 times for now    
 
 %% Create old particles
 old_positions = placing_particles(N, R, L);                         % Creates old positions

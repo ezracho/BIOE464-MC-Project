e8b3984610fe96f0ac23de74d2208a2dc1c89fea
ezracho <ezracho2000@gmail.com>
2020-10-27 11:04:07 -0400

    Changed to not check overlaps after initial point placement

--- pinpoint_change.m
+++ pinpoint_change.m
@@ -5,10 +5,14 @@ function [new_pos] = pinpoint_change(old_pos, N, R, L)
 % of how the overlap function is currently coded
 swap_pos = randi(N);            % Used to alter position coordinates
 old_pos(swap_pos, :) = [];             % Omit that row (now N-1 points)
-q = true;
-while q == true
-    gen_pos = [(L*rand(1)-0.5), (L*rand(1)-0.5), (L*rand(1)-0.5)]; %create new position
-    new_pos = [old_pos; gen_pos];                                  % Append new position
-    q = overlap(new_pos, R, L);                                       % Determines whether to repeat this loop
-end
+
+gen_pos = [(L*rand(1)-0.5), (L*rand(1)-0.5), (L*rand(1)-0.5)]; %create new position
+new_pos = [old_pos; gen_pos];
+% No need to check for overlaps anymore
+% q = true;
+% while q == true
+%     gen_pos = [(L*rand(1)-0.5), (L*rand(1)-0.5), (L*rand(1)-0.5)]; %create new position
+%     new_pos = [old_pos; gen_pos];                                  % Append new position
+%     q = overlap(new_pos, R, L);                                       % Determines whether to repeat this loop
+% end
 % Exiting loop means no overlap

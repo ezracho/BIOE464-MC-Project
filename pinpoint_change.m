bbf846f5fd9654474500d9c20dc490873b6ca674
ezracho <ezracho2000@gmail.com>
2020-10-31 14:49:07 -0400

    Tweaked point placement

--- pinpoint_change.m
+++ pinpoint_change.m
@@ -7,7 +7,7 @@ swap_pos = randi(N);            % Used to alter position coordinates
 old_pos(swap_pos, :) = [];             % Omit that row (now N-1 points)
 q = true;
 while q == true
-    gen_pos = [(L*rand(1)-0.5), (L*rand(1)-0.5), (L*rand(1)-0.5)]; %create new position
+    gen_pos = [(L*rand(1)-(L/2)), (L*rand(1)-(L/2)), (L*rand(1)-(L/2))]; %create new position
     new_pos = [old_pos; gen_pos];                                  % Append new position
     q = overlap(new_pos, R, L);                                       % Determines whether to repeat this loop
 end
